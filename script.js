a4b436ef1319642f6967b64626f75891658cb157
bonny-chen81 <bonnychen.tw@gmail.com>
2024-09-05 23:49:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ var map = L.map('map').setView([25.033, 121.565], 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
-
-// 加入災害潛勢區域 (臺北市)
-var taipeiFloodRisk = L.polygon([
-  [25.033, 121.565],
-  [25.043, 121.575],
-  [25.053, 121.555]
-]).addTo(map).bindPopup("臺北市淹水潛勢區域");
